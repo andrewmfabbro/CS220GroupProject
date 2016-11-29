c9a135f63c22cbada93d0b6cea241fa70941c5a4
ericghaly <ericghaly@gmail.com>
2016-11-29 10:24:43 -0500

    Update BookStore.cpp
    Files from Nov. 29

--- BookStore.cpp
+++ BookStore.cpp
@@ -11,6 +11,8 @@
 BookStore::BookStore() {
     this->capacity = 0;
     this->totalLinesRun = 1;
+    this->start = nullptr;
+    this->end = nullptr;
 }
 
 BookStore::~BookStore() {
@@ -38,12 +40,102 @@ bool BookStore::compareBooks(std::string existing, std::string newBook){
     return false;
 }
 
-void BookStore::add(int itemToAdd, int index) {
-    //TODO: add add function
+void BookStore::add(std::string titleToAdd, int have, int want) {
+    Book* bookToAdd = new Book(titleToAdd, have, want);
+    if(capacity == 0){
+        start = bookToAdd;
+        capacity++;
+    } else if(capacity == 1){
+        if(compareBooks(start->getTitle(), titleToAdd)){
+            end = start;
+            start = bookToAdd;
+            bookToAdd->setNext(end);
+            capacity++;
+        } else {
+            start->setNext(bookToAdd);
+            end = bookToAdd;
+            capacity++;
+        }
+    } else {
+        Book* currBook = start;
+        Book* oldBook = currBook->getNext();
+        for(int i = 0; i < capacity; i++){
+            if(compareBooks(currBook->getTitle(), titleToAdd)){
+                bookToAdd->setNext(currBook);
+                oldBook->setNext(bookToAdd);
+                capacity+=1;
+                return;
+            } else {
+                oldBook = currBook;
+                currBook = currBook->getNext();
+            }
+        }
+        end->setNext(bookToAdd);
+        end = bookToAdd;
+        capacity+=1;
+    }
 }
 
 std::string BookStore::remove(std::string title){
-    //TODO: add remove function
+    if(capacity == 0) {
+        return "There are currently no books to remove!";
+    }
+    else if(capacity == 1){
+        if(start->getTitle() == title){
+            std::string deletedTitle = start->getTitle();
+            delete start;
+            start = nullptr;
+            capacity--;
+            return deletedTitle;
+        } else {
+            return title + " is not in your bookstore!";
+        }
+    } else if(capacity == 2){
+        if(start->getTitle() == title){
+            std::string deletedTitle = start->getTitle();
+            delete start;
+            start = end;
+            end = nullptr;
+            capacity--;
+            return deletedTitle;
+        } else if(end->getTitle() == title){
+            std::string deletedTitle = end->getTitle();
+            delete end;
+            end = nullptr;
+            start->setNext(nullptr);
+            capacity--;
+            return deletedTitle;
+        } else {
+            return title + " is not in your bookstore!";
+        }
+    } else {
+        if(start->getTitle() == title){
+            Book* nextBook = start->getNext();
+            std::string deletedTitle = start->getTitle();
+            delete start;
+            start = nextBook;
+            capacity--;
+            return deletedTitle;
+        }
+        Book* prevBook = start;
+        Book* currBook = prevBook->getNext();
+        Book* nextBook = currBook->getNext();
+        std::string deletedTitle = title + " is not in your bookstore!";
+        for(int i = 0; i < capacity; i++){
+            if(currBook->getTitle() == title){
+                deletedTitle = currBook->getTitle();
+                delete currBook;
+                prevBook->setNext(nextBook);
+                capacity--;
+                return deletedTitle;
+            } else {
+                prevBook = currBook;
+                currBook = nextBook;
+                nextBook = nextBook->getNext();
+            }
+        }
+        return deletedTitle;
+    }
 }
 
 bool BookStore::isEmpty() {
@@ -73,27 +165,45 @@ void BookStore::clearList() {
     capacity = 0;
 }
 
-std::string BookStore::toString(){
-    return "no";
+void BookStore::toString(){
+//    std::cout << capacity << std::endl;
+    Book* bookToPrint = start;
+    for(int i = 0; i < capacity; i++){
+        bookToPrint->toString();
+        bookToPrint = bookToPrint->getNext();
+    }
 }
 
-void BookStore::find(std::string itemToFind) {
-    std::string indexToFind = "Book Not Found!";
-    Book* nodeToSearch = start;
+bool BookStore::find(std::string itemToFind) {
+    bool bookFound = false;
+    Book* bookToSearch = start;
     totalLinesRun+=4;
     for(int i = 0; i < capacity; i++){
         totalLinesRun+=3;
-        if(nodeToSearch->getTitle() == itemToFind){
-            indexToFind = i;
+        if(bookToSearch->getTitle() == itemToFind){
             totalLinesRun+=2;
-            nodeToSearch->toString();
-            i = capacity;
+            bookFound = true;
+            return bookFound;
         }
-        nodeToSearch = nodeToSearch->getNext();
+        bookToSearch = bookToSearch->getNext();
     }
     totalLinesRun+=1;
-    std::cout << indexToFind << std::endl;
+    return bookFound;
 }
+
+Book* BookStore::getBook(std::string title){
+    Book* bookToSearch = start;
+    for(int i = 0; i < capacity; i++){
+        if(bookToSearch->getTitle() == title){
+            return bookToSearch;
+        } else {
+            bookToSearch = bookToSearch->getNext();
+        }
+    }
+    std::cout << "The requested title is not in your bookstore!" << std::endl;
+    return nullptr;
+}
+
 
 
 long BookStore::getTotalLinesRun() { return totalLinesRun; }
@@ -115,6 +225,34 @@ void BookStore::takeDelivery(){
 void BookStore::returnBooks(){
 
 }
+
+
+/*int main(){
+    BookStore* newStore = new BookStore();
+
+    newStore->add("c",10,0);
+    newStore->add("a",10,0);
+    newStore->add("g",10,0);
+    newStore->add("d",10,0);
+    newStore->add("f",10,0);
+    newStore->add("e",10,0);
+    newStore->add("b",10,0);
+    newStore->add("i",10,0);
+    newStore->add("h",10,0);
+    newStore->add("j",10,0);
+    newStore->add("k",10,0);
+
+
+   // std::cout << newStore->remove("a") << std::endl;
+    newStore->remove("k");
+    newStore->toString();
+
+    printf("\n");
+    std::cout<< newStore->find("w") << std::endl;
+    std::cout<< newStore->find("c") << std::endl;
+
+
+}*/
 
 
 
