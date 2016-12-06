646a8d373953502958d6c83f5740cb0401427ad8
ericghaly <ericghaly@gmail.com>
2016-12-06 12:29:24 -0500

    WaitList.cpp with updates

--- WaitList.cpp
+++ WaitList.cpp
@@ -2,21 +2,27 @@
 // Created by Toby Dragon on 10/21/16.
 //
 
+#include <iostream>
 #include "WaitList.h"
-
 
 WaitList::WaitList(){
     currCapacity = 20;
-    array = new std::string[currCapacity];
+    array;
     currItemCount=0;
     totalLinesRun=3;
+    active = false;
 }
 
 WaitList::~WaitList(){
     delete[] array;
+    active = false;
 }
 
 void WaitList::enqueue(std::string item){
+    if(!active){
+        array = new std::string[currCapacity];
+        active = true;
+    }
     array[currItemCount] = item;
     currItemCount++;
     totalLinesRun+=2;
@@ -31,6 +37,9 @@ std::string WaitList::dequeue(){
     }
     currItemCount--;
     totalLinesRun+=2;
+    if(isEmpty()){
+        delete array;
+    }
     return item;
 }
 
@@ -49,4 +58,22 @@ void WaitList::resetTotalLinesRun() {
 
 int WaitList::calcSizeOf() {
     return sizeof(array) + (sizeof(*array)*currCapacity) + sizeof(currCapacity) + sizeof(currItemCount);
+}
+
+void WaitList::printList(){
+    for(int i = 0; i < currItemCount; i++){
+        std::cout << *(array+i) << std::endl;
+    }
+}
+
+bool WaitList::isActive(){
+    return active;
+}
+
+std::string WaitList::list(){
+    std::string stringToReturn;
+    for(int i = 0; i < currItemCount; i++){
+        stringToReturn += ("," + *(array+i));
+    }
+    return stringToReturn;
 }
